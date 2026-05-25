cbaf702439d06aeaf26d58903de3ca119b0f762e
Nicolas Escudero <escuderonico0@gmail.com>
2026-05-25 16:52:49 -0300

    new ui/ux

--- src/app/sneaker-list/sneaker-list.ts
+++ src/app/sneaker-list/sneaker-list.ts
@@ -11,31 +11,91 @@ import { SneakerCart } from '../sneaker-cart';
 export class SneakerList {
   sneakers: Sneaker[] = [{
     id: 1,
-    name: 'Jordan 1',
-    type: 'Deportiva',
+    name: 'Jordan 1 Off White Chicago',
+    type: 'Nike',
     price: 180,
     stock: 5,
-    image: 'assets/img/jordan1.jpg',
+    image: 'assets/img/Jordan-1-Off-White-chicago.webp',
     clearance: false,
     quantity: 0
   },
   {
     id: 2,
     name: 'Vans Knu',
-    type: 'Urbanas',
+    type: 'Vans',
     price: 200,
     stock: 3,
-    image: 'assets/img/vansknu.jpg',
+    image: 'assets/img/vansknu.webp',
     clearance: true,
     quantity: 0
   },
   {
     id: 3,
-    name: 'Converse Chuck',
-    type: 'Clásicas',
+    name: 'Sb dunk Strangelove',
+    type: 'Nike',
     price: 180,
     stock: 0,
-    image: 'assets/img/conversechuck.jpg',
+    image: 'assets/img/sb-dunck-love.webp',
+    clearance: false,
+    quantity: 0
+  },
+  {
+    id: 4,
+    name: 'Jordan 1 Off White Blue',
+    type: 'Nike',
+    price: 190,
+    stock: 2,
+    image: 'assets/img/Jordan-1-Off-White-blue.webp',
+    clearance: true,
+    quantity: 0
+  },
+  {
+    id: 5,
+    name: 'Vans knu Marrones',
+    type: 'Vans',
+    price: 180,
+    stock: 0,
+    image: 'assets/img/vansknu-marrones.webp',
+    clearance: false,
+    quantity: 0
+  },
+  {
+    id: 6,
+    name: 'Sb dunk Travis Scott',
+    type: 'Nike',
+    price: 300,
+    stock: 7,
+    image: 'assets/img/Nike-SB-Dunk-Low-Travis-Scott.webp',
+    clearance: false,
+    quantity: 0
+  },
+  {
+    id: 7,
+    name: 'Jordan 1 Dia de los muertos',
+    type: 'Nike',
+    price: 190,
+    stock: 2,
+    image: 'assets/img/Jordan-1-dia-de-los-muertos.webp',
+    clearance: true,
+    quantity: 0
+  },
+  {
+    id: 8,
+    name: 'Vans knu verdes',
+    type: 'Vans',
+    price: 180,
+    stock: 2,
+    image: 'assets/img/vansknu-verde.webp',
+    clearance: false,
+    quantity: 0
+  },
+  {
+    id: 9,
+    name: 'Sb dunk Futura',
+    type: 'Nike',
+    price: 280,
+    stock: 0,
+    image: 'assets/img/Nike-SB-Dunk-futura.webp',
     clearance: false,
     quantity: 0
   }];
@@ -49,7 +109,13 @@ export class SneakerList {
     sneaker.quantity = 0;
   }
 
+  alertMessage: string = '';
+
   maxReached(message: string): void {
-    alert(message);
+    this.alertMessage = message;
+
+    setTimeout(() => {
+      this.alertMessage = '';
+    }, 3000);
   }
 }
